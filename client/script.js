214bbc435640a2f3ec748eca3939f37e2f53ebb8
Parikshit Shahade <parikshitshahade@gmail.com>
2023-03-14 00:07:21 +0530

    changed the server

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ function loader(element) {
         loader(messageDiv);
 
         
-        const response = await fetch('https://localhost:5000',{
+        const response = await fetch('https://soupgpt.onrender.com',{
 
         method:'POST',
         headers: {
