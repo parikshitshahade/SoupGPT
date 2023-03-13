8a432b1af27a80ecd9493ea680bc48e196e60d86
Parikshit Shahade <parikshitshahade@gmail.com>
2023-03-14 00:00:33 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ function loader(element) {
         loader(messageDiv);
 
         
-        const response = await fetch('https://soupgpt.onrender.com',{
+        const response = await fetch('https://localhost:5000',{
 
         method:'POST',
         headers: {
